025564a337985570e89e45ea7eb816e202adc56c
ypablo <yfedora@gmail.com>
2019-06-05 01:31:55 +0100

    20.7 HMR

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -5,19 +5,27 @@ import style from '../containers/App.css';
 class TodoForm extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            clearInput: "" 
+        }
     }
+
+    updateInput(event) {
+        this.setState({[event.target.name]:event.target.value});
+      }
 
     addItem(event) {
         event.preventDefault();
-        const el = document.querySelector('input').value;
+        let el = document.querySelector('input').value;  
         this.props.addTodo(el);
+        this.setState({clearInput: ""});
     }
 
     render() {
         return (
             <div className={style.TodoForm}>
                 <form onSubmit={this.addItem.bind(this)}>
-                    <input type="text" placeholder="Add a task"></input>
+                    <input type="text" placeholder="Add a task" name="clearInput" value={this.state.clearInput} onChange={this.updateInput.bind(this)}></input>
                     <button type="submit">Add</button>
                 </form>
             </div>
